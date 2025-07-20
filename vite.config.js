568a27ea78abed356eab6d8ccf4123b9edcb5492
Narottam Jha <narottam2911@gmail.com>
2025-07-20 21:15:04 +0530

    Dummy commit to test build and stop duplication of actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 import {resolve} from "path"
-
+// 
 export default defineConfig({
     base: '/JS-Scribbler/',
     plugins: [react()],
